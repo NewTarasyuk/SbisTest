ba6390186e0ea50f6ebee3c8ca23bcf5a96d7ff8
Net <tarasyuk93@inbox.ru>
2024-03-17 21:04:17 +0500

    Обновление

--- test_sbis1.py
+++ test_sbis1.py
@@ -3,7 +3,6 @@ import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class TestUntitled():
